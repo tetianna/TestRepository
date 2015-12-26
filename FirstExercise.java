9e1517b4ce735bd79f70923f01aa08426644be4e
Tetianna <tetianna@gmail.com>
2015-12-25 20:59:01 -0800

    First and SEcond exercises codes

---
+++ FirstExercise.java
@@ -0,0 +1,18 @@
+public class FirstExercise	
+{
+	public static void main(String[] args)
+	{
+	//Variables
+	int number1, number2, numbersum;
+	
+	
+	number1=10;
+	number2=20;
+	numbersum=number1+number2;
+	
+	System.out.println("Sum of variable 1 ("+number1+") and variable 2 ("+number2+") is "+numbersum+"");
+	
+	}	
+		
+}
+
---
+++ SecondExercise.java
@@ -0,0 +1,22 @@
+public class SecondExercise	
+{
+	public static void main(String[] args)
+	{
+	//Variables
+	int a,b;
+	
+	
+	a=25;
+	b=35;
+	
+	
+	System.out.println("Before SWAP A: "+a+" and B: "+b+"");
+	
+	a=35;
+	b=25;
+	
+	System.out.println("After SWAP A: "+a+" and B: "+b+"");
+	}	
+		
+}
+
